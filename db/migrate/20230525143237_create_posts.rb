bd438c7c6e16622ff36a0928990969632db715c2
EC2 Default User <ec2-user@ip-172-31-32-215.ap-northeast-1.compute.internal>
2023-05-25 14:41:48 +0000

    updata

---
+++ db/migrate/20230525143237_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      
+      t.integer :user_id,     null: false
+      t.text    :posted_text, null: false
+      t.text    :sentence,    null: false
+
+      t.timestamps
+    end
+  end
+end
