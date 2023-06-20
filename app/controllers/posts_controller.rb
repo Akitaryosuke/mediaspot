0a9634547aa9d50580b9faec0bb46dc95351d03d
EC2 Default User <ec2-user@ip-172-31-32-215.ap-northeast-1.compute.internal>
2023-06-20 08:22:11 +0000

    updata

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,8 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
+    @post.user_id = current_user.id
+    @post.save!
     redirect_to root_path
   end
 
